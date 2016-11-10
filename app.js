474d3c1c56750d5c853bcad9537586a27fb3f8d2
Lonk <rinke2005@gmail.com>
2016-11-10 20:07:00 +0100

    chore(proxy): add proxy

---
+++ app.js
@@ -0,0 +1,46 @@
+const webpack   = require('webpack');
+const express   = require('express');
+const proxy     = require('http-proxy-middleware');
+const fs        = require('fs');
+
+const app      = express();
+const compiler = webpack(require('./webpack.config.js'));
+
+const options = {
+    target: {
+        host    : 'localhost',
+        port    : 3000,
+        protocol: 'https:',
+        ca      : fs.readFileSync('./ssl/ca-crt.pem', 'utf8'),
+        cert    : fs.readFileSync('./ssl/client-crt.pem', 'utf8'),
+        key     : fs.readFileSync('./ssl/client-key.pem', 'utf8')
+    },
+    changeOrigin: true,
+    secure: false,
+    pathRewrite: {
+         '^/api' : '/services/manager'
+    },
+    logLevel: 'warn'
+};
+
+compiler.watch({
+    aggregateTimeout: 300,
+    poll: true
+}, (err, stats) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log(stats.toString({
+        chunks: false,
+        colors: true
+    }));
+});
+
+app.use('/', express.static('public'));
+
+app.use('/api/*', proxy(options));
+
+app.listen(8083, () => {
+    console.log('Server listening on port 8083');
+});
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        Home
+        Home2
     </div>
 </template>
